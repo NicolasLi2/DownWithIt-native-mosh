92deba7b2053b16cf05d29e49d6130fafd8ffd8b
nicolas <linze201@gmail.com>
2024-02-18 22:51:15 +0800

    delete app.json => "orientation":"portrait",

--- App.tsx
+++ App.tsx
@@ -25,8 +25,8 @@ export default function App() {
         style={{
           backgroundColor: 'dodgerblue',
           // width: 150,
-          width: '50%',
-          height: 70,
+          width: '100%',
+          height: '30%',
         }}
       ></View>
     </SafeAreaView>
