83ba7ef3a7314482f088145a3684b3f90756697e
nicolas <linze201@gmail.com>
2024-02-18 20:53:35 +0800

    numberOfLines, onPress

--- App.tsx
+++ App.tsx
@@ -2,10 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+    console.log('Text pressed');
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text numberOfLines={1} onPress={handlePress}>
+        Open up App.tsx to start working on Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Ad, tenetur, sint neque accusantium, rerum
+        nemo ipsam consequuntur facere eveniet in ea earum molestias corrupti
+        accusamus! Asperiores rem corporis officia laborum.
+      </Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
