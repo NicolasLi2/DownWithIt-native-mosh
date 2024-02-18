eab1dc350ed15713b4c8aacf166f8736830704ad
nicolas <linze201@gmail.com>
2024-02-18 21:23:21 +0800

    Image components

--- App.tsx
+++ App.tsx
@@ -2,11 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-  console.log(require('./assets/favicon.png'));
+  // console.log(require('./assets/favicon.png'));
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello</Text>
-      <Image source={require('./assets/splash.png')} />
+      {/* <Image source={require('./assets/splash.png')} /> */}
+      <Image
+        blurRadius={10}
+        fadeDuration={1000}
+        source={{
+          width: 200,
+          height: 300,
+          uri: 'https://picsum.photos/200/300',
+        }}
+      />
       <StatusBar style='auto' />
     </SafeAreaView>
   );
