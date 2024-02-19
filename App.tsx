c89d7615d2fc1db10e8086c6a0e9121b7d389c71
nicolas <linze201@gmail.com>
2024-02-19 20:49:22 +0800

    create AppButton file

--- App.tsx
+++ App.tsx
@@ -1,16 +1,5 @@
-import { View } from 'react-native';
-import AppText from './app/components/AppText';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <AppText>I love React Native!</AppText>
-    </View>
-  );
+  return <WelcomeScreen />;
 }
