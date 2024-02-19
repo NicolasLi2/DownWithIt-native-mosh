823e39c400ac2e65c407b1fda1faa7771afbeb28
nicolas <linze201@gmail.com>
2024-02-19 18:39:52 +0800

    add platform specific code in AppText

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { Text, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import AppText from './app/components/AppText';
 
 export default function App() {
   return (
@@ -10,7 +11,7 @@ export default function App() {
         alignItems: 'center',
       }}
     >
-      <MaterialCommunityIcons name='email' size={60} color='dodgerblue' />
+      <AppText>I love React Native!</AppText>
     </View>
   );
 }
