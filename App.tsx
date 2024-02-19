bef37cc4a3cf8af82beff555635c4b17f0b1079b
nicolas <linze201@gmail.com>
2024-02-19 16:54:29 +0800

    style: border

--- App.tsx
+++ App.tsx
@@ -1,7 +1,28 @@
+import { View } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  // return <WelcomeScreen />;
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: 100,
+          height: 100,
+          borderWidth: 10,
+          borderColor: 'royalblue',
+          borderRadius: 10,
+          borderTopWidth: 20,
+          borderTopLeftRadius: 50,
+        }}
+      ></View>
+    </View>
+  );
 }
