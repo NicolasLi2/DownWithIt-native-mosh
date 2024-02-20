a6dfa0457549a37d7c4ea93ac879d04c90b94c95
nicolas <linze201@gmail.com>
2024-02-20 12:40:51 +0800

    create Icon component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { View } from 'react-native';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import Card from './app/components/Card';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
+import Icon from './app/components/Icon';
+import Screen from './app/components/Screen';
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name='email' size={50} backgroundColor='red' iconColor='white' />
+    </Screen>
+  );
 }
