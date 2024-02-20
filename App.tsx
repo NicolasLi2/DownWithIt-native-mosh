8d239c682bb1f7e38e18ad198431f6cadd2c8e35
nicolas <linze201@gmail.com>
2024-02-20 15:48:59 +0800

    implement TextInput

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
-import Screen from './app/components/Screen';
-import AccountScreen from './app/screens/AccountScreen';
+import { Text, TextInput } from 'react-native';
 import ListingsScreen from './app/screens/ListingsScreen';
+import Screen from './app/components/Screen';
+import { useState } from 'react';
 
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState('');
+
+  return (
+    <Screen>
+      <Text>{firstName}</Text>
+      <TextInput
+        keyboardType='numeric'
+        secureTextEntry
+        clearButtonMode='always'
+        onChangeText={(text) => setFirstName(text)}
+        placeholder='First Name'
+        style={{
+          borderBottomColor: '#ccc',
+          borderBottomWidth: 1,
+        }}
+      />
+    </Screen>
+  );
 }
