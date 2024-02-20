9e536ce9225c1634990c862d450b0fd0a5a0889c
nicolas <linze201@gmail.com>
2024-02-20 10:06:09 +0800

    create ListItemSeparator

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -23,6 +23,7 @@ export default function ListItem({ title, subTitle, image }: ListItemProps) {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
+    padding: 15,
   },
   image: {
     width: 70,
