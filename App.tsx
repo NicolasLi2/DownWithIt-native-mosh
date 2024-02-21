8f10157c0d642edd211c94f0337fb7ef5e42b255
nicolas <linze201@gmail.com>
2024-02-21 13:41:46 +0800

    add select image functionality

--- App.tsx
+++ App.tsx
@@ -7,9 +7,13 @@ import ListingEditScreen from './app/screens/ListingEditScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+import { Button, Image } from 'react-native';
 
 export default function App() {
+  const [imageUri, setImageUri] = useState('');
   const requestPermission = async () => {
+    // const result = await Permissions.askAsync(Permissions.MEDIA_LIBRARY,Permissions.LOCATION_FOREGROUND)
     const { granted } = await ImagePicker.requestCameraPermissionsAsync();
     if (!granted) alert('You need to enable permission to access the library');
   };
@@ -17,5 +21,22 @@ export default function App() {
     requestPermission();
   }, []);
 
-  return <Screen></Screen>;
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled) setImageUri(result.assets[0].uri);
+    } catch (error) {
+      console.log('Error reading an image', error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title='Select Image' onPress={selectImage} />
+      <Image
+        source={imageUri ? { uri: imageUri } : undefined}
+        style={{ width: 200, height: 200 }}
+      />
+    </Screen>
+  );
 }
