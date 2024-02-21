627a9f134a8c15f99eba0e528eebd4a3cd05a2c5
nicolas <linze201@gmail.com>
2024-02-21 22:02:10 +0800

    create AccountNavigator

---
+++ app/navigation/AccountNavigator.tsx
@@ -0,0 +1,14 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AccountScreen from '../screens/AccountScreen';
+import MessagesScreen from '../screens/MessagesScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AccountNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Account' component={AccountScreen} />
+      <Stack.Screen name='Messages' component={MessagesScreen} />
+    </Stack.Navigator>
+  );
+}
