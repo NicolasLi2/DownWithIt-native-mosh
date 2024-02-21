84de58f2b4c04122042e8edf7dd4a53c45caad0b
nicolas <linze201@gmail.com>
2024-02-21 12:31:05 +0800

    using image picker and request permission

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AppPicker from './app/components/Picker';
 import AppTextInput from './app/components/TextInput';
 import Screen from './app/components/Screen';
@@ -6,7 +6,16 @@ import LoginScreen from './app/screens/LoginScreen';
 import ListingEditScreen from './app/screens/ListingEditScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
-  return <ListingEditScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert('You need to enable permission to access the library');
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  return <Screen></Screen>;
 }
