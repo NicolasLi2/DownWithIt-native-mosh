6680f93491219b367cd0014db0258c630353a882
nicolas <linze201@gmail.com>
2024-02-21 17:48:16 +0800

    createNativeStackNavigator

--- App.tsx
+++ App.tsx
@@ -8,12 +8,37 @@ import ListingsScreen from './app/screens/ListingsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
-import { Button, Image } from 'react-native';
+import { Button, Image, Text } from 'react-native';
 import ImageInput from './app/components/ImageInput';
 import ImageInputList from './app/components/ImageInputList';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => (
+  // <Stack.Navigator initialRouteName='Tweets'>
+  <Stack.Navigator>
+    <Stack.Screen name='Tweets' component={Tweets} />
+    <Stack.Screen name='TweetDetails' component={TweetDetails} />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  const [imageUris, setImageUris] = useState(['']);
-
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
